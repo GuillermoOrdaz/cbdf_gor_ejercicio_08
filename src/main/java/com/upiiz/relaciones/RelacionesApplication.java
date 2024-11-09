66344bfc16fb7692e47dbe2a60cf303316863efa
Guillermo <pinguino2223@gmail.com>
2024-11-08 21:42:49 -0600

    version1

---
+++ src/main/java/com/upiiz/relaciones/RelacionesApplication.java
@@ -0,0 +1,13 @@
+package com.upiiz.relaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.upiiz.relaciones"})
+public class RelacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelacionesApplication.class, args);
+	}
+
+}
